6c744a2247637179ca059df0096b4a48ce6bfba1
Do Hoerin <hellodhlyn@gmail.com>
2015-12-20 06:46:23 -0500

    [HOTFIX] 20151220

--- wiki/models.py
+++ wiki/models.py
@@ -13,7 +13,7 @@ class Article(models.Model):
         ordering = ['title']
 
     title = models.CharField(verbose_name=u'title', max_length=256)
-    subtitle = models.CharField(verbose_name=u'subtitle', max_length=256, default='')
+    subtitle = models.CharField(verbose_name=u'subtitle', max_length=256, null=True, default='')
     content = models.TextField(u'content', blank=True, default='')
     created = models.DateTimeField(auto_now_add=True, verbose_name=u'date')
     last_modified = models.DateTimeField(auto_now_add=True, null=True, verbose_name=u'modified')
@@ -47,7 +47,7 @@ def modify(request):
         return HttpResponse(status=201)
 
     req_title = request.POST['title']
-    req_content = request.POST['content']
+    req_content = filter_words(request.POST['content'])
 
     try:
         article = Article.objects.get(title=req_title)
@@ -62,3 +62,13 @@ def modify(request):
     new_history.save()
 
     return HttpResponseRedirect(reverse('wikiarticle', kwargs={'pk': req_title}))
+
+filter_keywords = ['<script>', '</script>', '<h1>', '</h1>', '<h2>', '</h2>', '<h3>', '</h3>', '<h4>', '</h4>', '<h5>', '</h5>', '<h6>', '</h6>', 
+                   '<a', '</a>', '<code>', '</code>', '<iframe>', '</iframe>', '<div>', '</div>', '<input>', '<textarea>', '</textarea>', '<style>', '</style>',
+		   '<br>', '<br/>', '<br />', '<img', '</img>', '<strong>', '</strong>', '<b>', '</b>', '<i>', '</i>', '<u>', '</u>']
+
+def filter_words(content):
+    for keyword in filter_keywords: 
+        content = content.replace(keyword, '')
+
+    return content
