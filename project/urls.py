818ffd34ce42b7b0f8bc9804967926b7154dedef
Do Hoerin <hellodhlyn@gmail.com>
2016-03-03 22:05:40 +0900

    robots.txt, requirements.txt 추가

--- project/urls.py
+++ project/urls.py
@@ -49,4 +49,5 @@ urlpatterns = [
 	url(r'^v1/media/upload/$', media.upload, name='api_mediaupload'),
 
 	# static:
+	url(r'^robots.txt/$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name='robots'),
 ]
