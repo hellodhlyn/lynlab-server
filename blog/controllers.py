e83f718d96cf8d930739d163c12d8785494d51dd
Do Hoerin <hellodhlyn@gmail.com>
2016-03-24 14:59:22 +0900

    hotfix. 비로그인 상태에서 블로그 게시글 안보이던 오류

--- blog/controllers.py
+++ blog/controllers.py
@@ -5,6 +5,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 from django_ajax.decorators import ajax
 
@@ -48,6 +49,7 @@ def create(request):
 
     return HttpResponseRedirect(reverse('blogadmin'))
 
+@csrf_exempt
 def load_posts(request):
     page = int(request.POST.get('page'))
     filters = request.POST.get('filters').split(',')[:-1]
