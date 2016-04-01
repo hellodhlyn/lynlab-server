6bc916ca0328ff5c613b2b3617fcb09fdb3333b6
Do Hoerin <hellodhlyn@gmail.com>
2016-04-01 17:49:26 +0900

    자동 새로고침 기능 추가

--- dashboard/static/dashboard.js
+++ dashboard/static/dashboard.js
@@ -1,15 +1,27 @@
+var reload_interval = 10;
+var reload_left = -1;
+
 $(document).ready(function() {
+	var reload_timer = setInterval(function () {
+		if (reload_left > 0) {
+			reload_left -= 1;
+		}
+		$('#reload-time').html(reload_left)
+	}, 1000);
+
 	// Request bus info
 	var req_url = '/dashboard/bus';
 	$.get(req_url, function(data) {
 		$('.dashboard-buses').html(data);
+		reload_left = 10;
 	});
 
-	var auto_refresh = setInterval(function () {
-	    $('.dashboard-buses').fadeOut('fast', function() {
-	        $(this).load('/dashboard/bus', function() {
-	            $(this).fadeIn('fast');
-	        });
-	    });
-	}, 10 * 1000);
+	setInterval(function () {
+		$('.dashboard-buses').fadeOut('fast', function() {
+			$(this).load('/dashboard/bus', function() {
+				$(this).fadeIn('fast');
+			});
+		});
+		reload_left = 10;
+	}, reload_interval * 1000);
 });
