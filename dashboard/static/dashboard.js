17dad401d52e1004c2bbce3c683de22be09a40fb
Do Hoerin <hellodhlyn@gmail.com>
2016-04-18 12:15:44 +0900

    버스 10분 이내 도착 노티 추가

--- dashboard/static/dashboard.js
+++ dashboard/static/dashboard.js
@@ -1,8 +1,55 @@
-var reload_interval = 10;
+const reload_interval = 15;
 var reload_left = -1;
+var notified_bus = [];
+
+// For notifactions
+function onShowNotification () {
+	console.log('notification is shown!');
+}
+function onCloseNotification () {
+	console.log('notification is closed!');
+}
+function onClickNotification () {
+	console.log('notification was clicked!');
+}
+function onErrorNotification () {
+	console.error('Error showing notification. You may need to request permission.');
+}
+function onPermissionGranted () {
+	console.log('Permission has been granted by the user');
+}
+function onPermissionDenied () {
+	console.warn('Permission has been denied by the user');
+}
+function doNotification (id, busNum, leftTime, people) {
+	if (notified_bus.includes(id)) return;
+	notified_bus.push(id);
+
+	var message = busNum + '번 버스가 ' + leftTime + ' 후에 도착합니다. (' + people + '명 탑승)';
+
+	var myNotification = new Notify('버스가 곧 도착합니다.', {
+		body: message,
+		tag: 'LYnDashboard_BUS',
+		icon: '/static/bus.png',
+		notifyShow: onShowNotification,
+		notifyClose: onCloseNotification,
+		notifyClick: onClickNotification,
+		notifyError: onErrorNotification,
+		timeout: 5
+	});
+	myNotification.show();
+}
 
 $(document).ready(function() {
-	var reload_timer = setInterval(function () {
+	$('body').append('<button id="permission-request-button"></button>');
+	$('#permission-request-button').click(function() {
+		if (Notify.needsPermission && Notify.isSupported()) {
+            Notify.requestPermission(onPermissionGranted, onPermissionDenied);
+        }
+	});
+	$('#permission-request-button').click();
+
+	var reload_timer = setInterval(function() {
 		if (reload_left > 0) {
 			reload_left -= 1;
 		}
@@ -13,7 +60,7 @@ $(document).ready(function() {
 	var req_url = '/dashboard/bus';
 	$.get(req_url, function(data) {
 		$('.dashboard-buses').html(data);
-		reload_left = 10;
+		reload_left = reload_interval;
 	});
 
 	setInterval(function () {
@@ -22,6 +69,6 @@ $(document).ready(function() {
 				$(this).fadeIn('fast');
 			});
 		});
-		reload_left = 10;
+		reload_left = reload_interval;
 	}, reload_interval * 1000);
 });
--- dashboard/views.py
+++ dashboard/views.py
@@ -21,13 +21,13 @@ def bus(request):
 		time_string = ''
 		if time >= 60:
 			time_string += str(time/60) + '분 '
-		time_string += '%02d' % (time%60) + '초 남음'
+		time_string += '%02d' % (time%60) + '초'
 
 		return time_string
 
 	lines_context = []
-
 	lines = BusDashboard.objects.all()
+	
 	for line in lines:
 		response_raw = bus_helper('arrive/getArrInfoByRouteAll', line.bus_id)
 		response = xmltodict.parse(response_raw)
@@ -37,6 +37,7 @@ def bus(request):
 		station_idx = station_list.index(station_now)
 
 		stations_context = []
+		notification_context = None
 		for i in range(7, -1, -1):
 			station = station_list[station_idx - i]
 
@@ -67,12 +68,22 @@ def bus(request):
 						'left_time': format_time(int(station_now['exps1'])),
 					}
 					stations_context.append(station_context)
+
+					# For notification
+					if int(station_now['exps1']) < 600:
+						notification_context = {
+							'bus_id': station['vehId1'],
+							'left_time': format_time(int(station_now['exps1'])),
+							'people': station['reride_Num1'],
+						}
+
 			
 		line_context = {
 			'bus_number': line.bus_number,
 			'station_name': line.station_name,
 			'terminus': station_now['dir'],
 			'stations': stations_context,
+			'notification': notification_context,
 		}
 
 		lines_context.append(line_context)
