12eaec7651cc0a25f66a82cc394f48baa4182474
Do Hoerin <hellodhlyn@gmail.com>
2016-04-30 16:55:44 +0900

    일시정지 기능 추가

--- dashboard/static/dashboard.js
+++ dashboard/static/dashboard.js
@@ -1,6 +1,8 @@
-const reload_interval = 15;
-var reload_left = -1;
+const reload_interval = 20;
+var reload_left = 0;
 var notified_bus = [];
+
+var isPaused = true;
 
 // For notifactions
 function onShowNotification () {
@@ -35,12 +37,21 @@ function doNotification (id, busNum, leftTime, people) {
 		notifyClose: onCloseNotification,
 		notifyClick: onClickNotification,
 		notifyError: onErrorNotification,
-		timeout: 5
+		timeout: 20
 	});
 	myNotification.show();
 }
 
 $(document).ready(function() {
+	function request() {
+		$('.dashboard-buses').fadeOut('fast', function() {
+			$(this).load('/dashboard/bus', function() {
+				$(this).fadeIn('fast');
+			});
+		});
+	}
+
+	// Notification auth
 	$('body').append('<button id="permission-request-button"></button>');
 	$('#permission-request-button').click(function() {
 		if (Notify.needsPermission && Notify.isSupported()) {
@@ -49,26 +60,31 @@ $(document).ready(function() {
 	});
 	$('#permission-request-button').click();
 
+	// Set timers
 	var reload_timer = setInterval(function() {
-		if (reload_left > 0) {
-			reload_left -= 1;
+		if (!isPaused) {
+			if (reload_left > 0) {
+				reload_left -= 1;
+			}
+			else {
+				request();
+				reload_left = reload_interval;
+			}
 		}
 		$('#reload-time').html(reload_left)
 	}, 1000);
 
-	// Request bus info
-	var req_url = '/dashboard/bus';
-	$.get(req_url, function(data) {
-		$('.dashboard-buses').html(data);
-		reload_left = reload_interval;
+	// Pause-resume toggle
+	$('#btn-pause').click(function() {
+		if ($('#btn-pause').children().hasClass('pause')) {
+			isPaused = true;
+			$('#btn-pause').html('<i class="play icon"></i>');
+		}
+		else {
+			isPaused = false;
+			$('#btn-pause').html('<i class="pause icon"></i>');
+		}
 	});
 
-	setInterval(function () {
-		$('.dashboard-buses').fadeOut('fast', function() {
-			$(this).load('/dashboard/bus', function() {
-				$(this).fadeIn('fast');
-			});
-		});
-		reload_left = reload_interval;
-	}, reload_interval * 1000);
+	request();
 });
