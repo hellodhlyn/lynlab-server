0f115c17d69b40c8ca7e296a41584d6131b047fa
Do Hoerin <hellodhlyn@gmail.com>
2016-10-02 15:38:12 +0900

    코드 정리

--- project/urls.py
+++ project/urls.py
@@ -21,60 +21,60 @@ import media.models as media
 import simple_wiki.models as WikiModel
 
 sitemaps = {
-	'sitemaps': {
-		'blog': GenericSitemap({
-			'queryset': blog.Post.objects.all(),
-			'date_field': 'created',
-		}, changefreq='monthly'),
-		'wiki': GenericSitemap({
-			'queryset': WikiModel.Article.objects.all(),
-			'date_field': 'created',
-		}, changefreq='weekly'),
-	},
+    'sitemaps': {
+        'blog': GenericSitemap({
+            'queryset': blog.Post.objects.all(),
+            'date_field': 'created',
+        }, changefreq='monthly'),
+        'wiki': GenericSitemap({
+            'queryset': WikiModel.Article.objects.all(),
+            'date_field': 'created',
+        }, changefreq='weekly'),
+    },
 }
 
 urlpatterns = [
-	# global:
-	url(r'^$', TemplateView.as_view(template_name='welcome.html'), name='home'),
-	url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    # global:
+    url(r'^$', TemplateView.as_view(template_name='welcome.html'), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 
-	# blog:
-	url(r'^blog/$', BlogView.main, name='blog'),
-	url(r'^blog/myadmin/$', BlogAdmin.admin, name='blogadmin'),
-	url(r'^blog/myadmin/create/tweet/$', BlogAdmin.create_tweet, name='blog-admin-create-tweet'),
-	url(r'^blog/myadmin/modify/$', BlogAdmin.create_post, name='blog-admin-create-post'),
-	url(r'^blog/myadmin/modify/(?P<pk>\d+)$', BlogAdmin.modify_post, name='blog-admin-modify-post'),
+    # blog:
+    url(r'^blog/$', BlogView.main, name='blog'),
+    url(r'^blog/myadmin/$', BlogAdmin.admin, name='blogadmin'),
+    url(r'^blog/myadmin/create/tweet/$', BlogAdmin.create_tweet, name='blog-admin-create-tweet'),
+    url(r'^blog/myadmin/modify/$', BlogAdmin.create_post, name='blog-admin-create-post'),
+    url(r'^blog/myadmin/modify/(?P<pk>\d+)$', BlogAdmin.modify_post, name='blog-admin-modify-post'),
     url(r'^blog/myadmin/series/$', BlogAdmin.series, name='blog-admin-create-series'),
     url(r'^blog/myadmin/series/modify/(?P<id>\d+)/$', BlogAdmin.modify_series, name='blog-admin-modify-series'),
-	url(r'^blog/(?P<pk>\d+)/$', BlogView.post_detail, name='detail'),
-	# url(r'^blog/', include('blog.urls')),
+    url(r'^blog/(?P<pk>\d+)/$', BlogView.post_detail, name='detail'),
+    # url(r'^blog/', include('blog.urls')),
 
-	# dashboard:
-	url(r'^dashboard/$', DashboardView.dashboard, name='dashboard'),
-	url(r'^dashboard/bus$', DashboardView.bus, name='dashboard-bus'),
+    # dashboard:
+    url(r'^dashboard/$', DashboardView.dashboard, name='dashboard'),
+    url(r'^dashboard/bus$', DashboardView.bus, name='dashboard-bus'),
 
-	# wiki:
-	url(r'^wiki/', include('simple_wiki.urls')),
+    # wiki:
+    url(r'^wiki/', include('simple_wiki.urls')),
 
-	# media:
-	url(r'^media/upload/$', upload_view, name='mediaupload'),
-	url(r'^media/(?P<pk>[\w|\W]+)/$', show_media, name='mediashow'),
+    # media:
+    url(r'^media/upload/$', upload_view, name='mediaupload'),
+    url(r'^media/(?P<pk>[\w|\W]+)/$', show_media, name='mediashow'),
 
-	# admin:
-	url(r'^admin/', include(admin.site.urls)),
+    # admin:
+    url(r'^admin/', include(admin.site.urls)),
 
-	# member:
-	#url(r'^accounts/', include('django.contrib.auth.urls')),
-	url(r'^accounts/profile/$', RedirectView.as_view(url='/wiki/')),
-	url(r'^accounts/', include('registration.backends.hmac.urls')),
+    # member:
+    #url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/profile/$', RedirectView.as_view(url='/wiki/')),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
 
-	# apis:
-	url(r'^api/blog/posts/$', BlogControl.load_posts, name='api-blog-posts'),
-	url(r'^api/blog/like/(?P<id>\d+)/$', BlogControl.like_post, name='api-blog-like-post'),
-	url(r'^api/blog/unlike/(?P<id>\d+)/$', BlogControl.unlike_post, name='api-blog-unlike-post'),
-	url(r'^v1/media/upload/$', media.upload, name='api_mediaupload'),
+    # apis:
+    url(r'^api/blog/posts/$', BlogControl.load_posts, name='api-blog-posts'),
+    url(r'^api/blog/like/(?P<id>\d+)/$', BlogControl.like_post, name='api-blog-like-post'),
+    url(r'^api/blog/unlike/(?P<id>\d+)/$', BlogControl.unlike_post, name='api-blog-unlike-post'),
+    url(r'^v1/media/upload/$', media.upload, name='api_mediaupload'),
 
-	# static:
-	url(r'^robots.txt/$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name='robots'),
-	url(r'^sitemap\.xml$', sitemap, sitemaps, name='django.contrib.sitemaps.views.sitemap'),
+    # static:
+    url(r'^robots.txt/$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name='robots'),
+    url(r'^sitemap\.xml$', sitemap, sitemaps, name='django.contrib.sitemaps.views.sitemap'),
 ]
