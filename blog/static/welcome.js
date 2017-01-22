59ccd4b944ecaaf123608a78397bce7ca94fa12a
Do Hoerin <hellodhlyn@gmail.com>
2017-01-22 23:10:31 +0900

    포트폴리오 업데이트

--- blog/static/welcome.js
+++ blog/static/welcome.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
 	$('.ui.dropdown').dropdown();
+
+	$('#sidebar-icon').on('click', function() {
+        $("#main-sidebar")
+            .sidebar('setting', 'transition', 'overlay')
+            .sidebar('toggle');
+	});
 });
