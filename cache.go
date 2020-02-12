502279f95efde42971e73a9c2471c39fcb4de079
Do Hoerin <hellodhlyn@gmail.com>
2020-02-12 23:25:28 +0900

    Good bye, golang!

--- cache.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"time"
-
-	cachelib "github.com/patrickmn/go-cache"
-)
-
-var cache = cachelib.New(24*time.Hour, 1*time.Hour)
--- graphql_helper.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/graphql-go/graphql"
-)
-
-func NonNullListOf(objType graphql.Type) graphql.Type {
-	return graphql.NewNonNull(graphql.NewList(graphql.NewNonNull(objType)))
-}
