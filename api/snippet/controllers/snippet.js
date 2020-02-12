b1bad0a5500e97db5a6edd2da13e0178ea32fb53
Do Hoerin <hellodhlyn@gmail.com>
2020-02-13 03:31:20 +0900

    Adding raw snippet api and some permission filters

--- api/snippet/controllers/snippet.js
+++ api/snippet/controllers/snippet.js
@@ -2,23 +2,38 @@
 
 const { sanitizeEntity } = require('strapi-utils');
 
+async function findSnippets(ctx) {
+  const { user } = ctx.state;
+  if (!user || !user.confirmed || (user.role.type !== 'authenticated')) {
+    ctx.query = { ...ctx.query, status: 'published' };
+  }
+
+  let entities;
+  if (ctx.query._q) {
+    entities = await strapi.services.snippet.search(ctx.query);
+  } else {
+    entities = await strapi.services.snippet.find(ctx.query);
+  }
+
+  return entities;
+}
+
 module.exports = {
   async find(ctx) {
-    let entities;
+    const entities = await findSnippets(ctx);
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.snippet }));
+  },
 
-    ctx.query = {
-      ...ctx.query,
-      status: 'published',
-    };
+  async findOne(ctx) {
+    const entities = await findSnippets(ctx);
+    const entity = entities.length > 0 ? entities[0] : null;
+    return sanitizeEntity(entity, { model: strapi.models.snippet });
+  },
 
-    if (ctx.query._q) {
-      entities = await strapi.services.snippet.search(ctx.query);
-    } else {
-      entities = await strapi.services.snippet.find(ctx.query);
+  async findOneRaw(ctx) {
+    const entities = await findSnippets(ctx);
+    if (entities.length > 0) {
+      return entities[0].body;
     }
-
-    return entities.map(entity =>
-      sanitizeEntity(entity, { model: strapi.models.snippet })
-    );
   },
 };
